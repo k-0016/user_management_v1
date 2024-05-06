b4d96f158d05eb7ff053575347c69f7af24f36b9
k-0016 <kp866@njit.edu>
2024-05-06 15:56:33 -0400

    Fixed The character limit

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -20,8 +20,8 @@ def validate_url(url: Optional[str]) -> Optional[str]:
 class UserBase(BaseModel):
     email: EmailStr = Field(..., example="john.doe@example.com")
     nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=generate_nickname())
-    first_name: Optional[str] = Field(None, example="John")
-    last_name: Optional[str] = Field(None, example="Doe")
+    first_name: Optional[str] = Field(None, max_length=100, example="John")
+    last_name: Optional[str] = Field(None, max_length=100, example="Doe")
     bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
     profile_picture_url: Optional[str] = Field(None, example="https://example.com/profiles/john.jpg")
     linkedin_profile_url: Optional[str] =Field(None, example="https://linkedin.com/in/johndoe")
